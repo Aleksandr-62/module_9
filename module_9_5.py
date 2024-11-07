a35140982384caf452d8ba2c0c2806419912e600
Александр <shurik-zet@mail.ru>
2024-11-07 17:22:32 +0300

    Add files via upload

---
+++ module_9_5.py
@@ -0,0 +1,68 @@
+
+"""
+Создайте пользовательский класс исключения StepValueError, который наследуется от ValueError.
+Наследования достаточно, класс оставьте пустым при помощи оператора pass.
+"""
+
+
+class StepValueError(ValueError):
+    pass
+
+
+"""
+Создайте класс Iterator, который обладает следующими свойствами:
+ Атрибуты объекта:
+ 1. start - целое число с которого начинается итерация.
+ 2. stop - целое число на котором заканчивается итерация.
+ 3. step - шаг с которой совершается итерация.
+ 4. pointer - указывает на текущее число в итерации (изначально start)
+"""
+
+
+class Iterator:
+    def __init__(self, start, stop, step=1):
+        if step == 0:
+            raise StepValueError('Шаг не может быть равен 0')
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.pointer = start
+
+    def __iter__(self):
+        self.pointer = self.start
+        return self
+
+    def __next__(self):
+        if (self.step > 0 and self.pointer > self.stop) or (self.step < 0 and self.pointer < self.stop):
+            raise StopIteration
+        current = self.pointer
+        self.pointer += self.step
+        return current
+
+
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+
+
+iter2 = Iterator(-5, 2)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+
+
+for i in iter2:
+    print(i, end=' ')
+print()
+for i in iter3:
+    print(i, end=' ')
+print()
+for i in iter4:
+    print(i, end=' ')
+print()
+for i in iter5:
+    print(i, end=' ')
+print()
